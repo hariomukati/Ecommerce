5f079e0f0ceb93ff12b73292ed4eeb8bb9705cd6
bhupendra.patidar42@gmail.com <bhupendrapatidar42@gmail.com>
2020-05-21 22:40:22 +0530

    Initial commit

---
+++ Ecommerce/src/main/java/in/co/ecommerce/form/UserRegistrationForm.java
@@ -0,0 +1,98 @@
+package in.co.ecommerce.form;
+
+import in.co.ecommerce.entity.BaseEntity;
+import in.co.ecommerce.entity.UserEntity;
+
+public class UserRegistrationForm extends BaseForm {
+
+	private String firstName;
+	private String lastName;
+	private String login;
+	private String password;
+	private String emailId;
+	private String mobileNo;
+	
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	@Override
+	public BaseEntity getBean() {
+		UserEntity bean=new UserEntity();
+		bean.setId(id);
+		bean.setFirstName(firstName);
+		bean.setLastName(lastName);
+		bean.setLoginId(login);
+		bean.setPassword(password);
+		bean.setEmailId(emailId);
+		bean.setMobileNo(mobileNo);
+		bean.setCreatedBy(createdBy);
+		bean.setModifiedBy(modifiedBy);
+		bean.setCreatedDateTime(createdDateTime);
+		bean.setModifiedDateTime(modifiedDateTime);
+		return bean;
+	}
+
+	@Override
+	public void populate(BaseEntity baseBean) {
+		UserEntity bean=(UserEntity)baseBean;
+		id=bean.getId();
+		firstName=bean.getFirstName();
+		lastName=bean.getLastName();
+		login=bean.getLoginId();
+		password=bean.getPassword();
+		emailId=bean.getEmailId();
+		mobileNo=bean.getMobileNo();
+		createdBy=bean.getCreatedBy();
+		modifiedBy=bean.getMobileNo();
+		createdDateTime=bean.getCreatedDateTime();
+		modifiedDateTime=bean.getModifiedDateTime();
+	}
+
+}
